151331fcb3805306f9fb85d3eb3d7f4964253737
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-08-26 18:51:31 +0600

    register inp

---
+++ src/pages/register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div className='h-screen bg-[#0046ff]'>
+            
+        </div>
+    );
+};
+
+export default Register;
